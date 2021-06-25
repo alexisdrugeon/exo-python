4683a4a2afea33eb7be41acaed381cceda64ca8e
alexisdrugeon <alexis.drugeon@gmail.com>
2021-06-25 12:57:02 +0200

    ca marche cette fois ???

---
+++ addition_python.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 9:22:59 2021
+
+@author: Alexis Drugeon
+"""
+
+#Objectif : faire une fonction python qui additionne deux chiffres entrés en console
+
+def addition_deux_chiffres():
+    a = int(input("a"))
+    b = int(input("b"))
+    return (a+b)
+
+addition_deux_chiffres()
+
+#zazfegfhdgfh,
