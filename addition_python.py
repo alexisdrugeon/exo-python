2a61e31838beedb17cc61a43b8d03591ae429918
alexisdrugeon <alexis.drugeon@gmail.com>
2021-06-25 13:08:20 +0200

    ajout de print

--- addition_python.py
+++ addition_python.py
@@ -8,8 +8,8 @@ Created on Fri Jun 25 9:22:59 2021
 #Objectif : faire une fonction python qui additionne deux chiffres entrés en console
 
 def addition_deux_chiffres():
-    a = int(input("a"))
-    b = int(input("b"))
+    a = int(input("premier nombre a additionner : "))
+    b = int(input("Second nombre a additionner : "))
     print("\n",a+b)
     return (a+b)
 
